307e250e4bd5a903c94ebe7da65418993d1c3010
goran-kovacic <kovacic.go@gmail.com>
2023-07-03 18:25:48 +0200

    Update TestLjubavi.java

--- Zadaci_sa_predavanja/Edunova06/src/edunova/TestLjubavi.java
+++ Zadaci_sa_predavanja/Edunova06/src/edunova/TestLjubavi.java
@@ -5,20 +5,32 @@ import javax.swing.JOptionPane;
 public class TestLjubavi {
 public static void main(String[] args) {
 	
-	String ime1 = JOptionPane.showInputDialog("unesi ime 1");
+	String name1 = JOptionPane.showInputDialog("unesi ime 1");
 	
-	String ime2 = JOptionPane.showInputDialog("unesi ime 2");
+	String name2 = JOptionPane.showInputDialog("unesi ime 2");
 	
-	String ime3 = ime1.concat(ime2);
+	String name3 = name1.concat(name2);
 	
 	int count=0;
 	
-	for(int i=0; i< ime3.length(); i++) {
-		if(ime3.charAt(i) != ' ')
+	for(int i=0; i< name3.length(); i++) {
+		if(name3.charAt(i) != ' ')
 			count++;
 	}
 	
-	System.out.println(count);
+}
+	
+	
+	static int rekurzija(String name3, char searchedChar, int index) {
+		if(index >= name3.length()) {
+			return 0;
+		}
+		int count = name3.charAt(index) == searchedChar ? 1 : 0;
+		return count + rekurzija(name3, searchedChar, index + 1);
+	
+	
+	
+	
 	
 	
 }
