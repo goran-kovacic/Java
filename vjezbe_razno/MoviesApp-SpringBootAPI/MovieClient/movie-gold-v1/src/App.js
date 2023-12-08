c39c4ddc5879708363f0933bc496e2c6bf2fdf08
goran-kovacic <kovacic.go@gmail.com>
2023-12-08 23:02:44 +0100

    end

--- vjezbe_razno/MoviesApp-SpringBootAPI/MovieClient/movie-gold-v1/src/App.js
+++ vjezbe_razno/MoviesApp-SpringBootAPI/MovieClient/movie-gold-v1/src/App.js
@@ -5,10 +5,15 @@ import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () =>{
 
@@ -22,6 +27,23 @@ function App() {
     
   }
 
+  const getMovieData = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+
+    } catch (error) {
+      
+    }
+
+  }
+
   useEffect(() =>{
     getMovies();
   },[] )
@@ -29,12 +51,12 @@ function App() {
 
   return (
     <div className="App">
-
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies} />} ></Route>
-
-
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
         </Route>
       </Routes>
       
