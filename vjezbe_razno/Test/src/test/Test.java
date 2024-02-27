2d2e823a76082c97496e97ac35a03d07fc365b9b
goran-kovacic <kovacic.go@gmail.com>
2024-02-27 17:20:08 +0100

    threads

--- vjezbe_razno/Test/src/test/Test.java
+++ vjezbe_razno/Test/src/test/Test.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
+import multithreading.MyRunnable;
+import multithreading.MyThread;
 import serialization.User;
 import java.io.*;
 
 
 public class Test {	
 	
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		
 //		User user = new User();
 //		
@@ -26,16 +28,29 @@ public class Test {
 //		
 //		System.out.println("object info saved");
 		
-		User user = null;
+//		User user = null;
+//		
+//		FileInputStream fileIn = new FileInputStream("userinfo.ser");
+//		ObjectInputStream in = new ObjectInputStream(fileIn);
+//		user = (User) in.readObject();
+//		in.close();
+//		fileIn.close();
+//		
+//		System.out.println(user.name + " " + user.password);
+//		user.sayHello();
 		
-		FileInputStream fileIn = new FileInputStream("userinfo.ser");
-		ObjectInputStream in = new ObjectInputStream(fileIn);
-		user = (User) in.readObject();
-		in.close();
-		fileIn.close();
 		
-		System.out.println(user.name + " " + user.password);
-		user.sayHello();
+		MyThread thread1 = new MyThread();
+		
+		MyRunnable runnable1 = new MyRunnable();
+		Thread thread2 = new Thread(runnable1);
+		
+		thread1.start();
+		thread1.join();
+		thread2.start();
+		
+		
+		
 	}
 	
 	
