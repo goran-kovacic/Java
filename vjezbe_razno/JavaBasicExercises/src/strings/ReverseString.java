a18024d638e8e9cf52a5a9bb3bdcc6a9b05f8cb1
goran-kovacic <kovacic.go@gmail.com>
2024-05-10 06:57:33 +0200

    Create ReverseString.java

---
+++ vjezbe_razno/JavaBasicExercises/src/strings/ReverseString.java
@@ -0,0 +1,26 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		
+		String inputString;
+		int inputStringLength;
+		StringBuilder sb = new StringBuilder();
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter a string: ");
+		inputString = s.next();
+		inputStringLength = inputString.length();
+		
+		for( int characterIndex = inputStringLength-1; characterIndex >=0; characterIndex--) {
+			sb.append(inputString.charAt(characterIndex));
+		}
+		
+		System.out.println("Reverse String: " + sb);
+		
+	}
+
+}
