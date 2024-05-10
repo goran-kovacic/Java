0a0d64bc1a5399ea8c2416c2f618f23a4c2d8a85
goran-kovacic <kovacic.go@gmail.com>
2024-05-10 06:43:46 +0200

    Update Factorial.java

--- vjezbe_razno/JavaBasicExercises/src/debugging/Factorial.java
+++ vjezbe_razno/JavaBasicExercises/src/debugging/Factorial.java
@@ -4,7 +4,7 @@ package debugging;
 public class Factorial {
 public static void main(String[] args) {  // Set an initial breakpoint at this statement
    int n = 20;
-   int factorial = 1;
+   long factorial = 1;
 
    // n! = 1*2*3...*n
    for (int i = 1; i <= n; i++) {  // i = 1, 2, 3, ..., n
